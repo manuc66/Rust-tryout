c17674003d27d7629c9f0c0ea7507c62705b1965
manuc66 <manuc66@gmail.com>
2014-11-23 15:23:57 +0100

    stringCalculator

---
+++ StringCalculator/stringCalculator_tests.rs
@@ -0,0 +1,49 @@
+fn compute(input: &str) -> int {
+	if is_empty(input) {
+		0
+	}
+	else {
+		sum_of(extract_numbers(input))
+	}
+}
+
+fn is_empty (input: &str) -> bool {
+	input.len() == 0
+}
+
+fn extract_numbers(input: &str) -> Vec<int> {
+	input.split_str(",").map(|x| parse_int(x)).collect()
+}
+
+fn parse_int(input: &str) -> int {
+	from_str(input).unwrap()
+}
+
+fn sum_of(numbers: Vec<int>) -> int {
+	let mut sum = 0i;
+	for &s in numbers.iter() {
+		sum = sum + s
+	}
+	sum
+}
+
+
+#[test]
+fn empty_got_zero() {
+	assert!(compute("") == 0);
+}
+
+#[test]
+fn one_got_one() {
+	assert!(compute("1") == 1);
+}
+
+#[test]
+fn eighteen_got_eighteen() {
+	assert!(compute("18") == 18);
+}
+
+#[test]
+fn one_plus_two_got_three() {
+	assert!(compute("1,2") == 3);
+}
